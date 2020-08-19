6a14f490001d0546e558a85d6cada58b6a2a2954
JohnOFarrell <johnofarrell12@gmail.com>
2020-08-19 11:44:22 -0400

    Added API Functionality
    Added an API connection to a coronavirus database, updated the inital template to make the data viewable

--- api test/public/javascripts/app.js
+++ api test/public/javascripts/app.js
@@ -5,20 +5,39 @@ app.controller('apiController', function($scope, $http) {
 
 
 
-  $scope.submitIds = function(){
+  $scope.getData = function(){
   	console.log("Test");
-  
-    $http({
-    method: 'GET',
-    url: '/someUrl'
-    }).then(function successCallback(response) {
-      // this callback will be called asynchronously
-      // when the response is available
-    }, function errorCallback(response) {
-      // called asynchronously if an error occurs
-      // or server returns response with an error status.
-    });
 
+    var corona = {
+     method: 'GET',
+     url: 'https://covid-193.p.rapidapi.com/statistics',
+     headers: {
+        "x-rapidapi-host": "covid-193.p.rapidapi.com",
+        "x-rapidapi-key": "4e69bba1d8msh08ff0f7abe94b29p15cb21jsnc162965f436c",
+        useQueryString : true
+      }
+    }
+
+    $http(corona).then(function successCallback(response) {
+      console.log("Success");
+      console.log(response);
+      console.log(response.data);
+      console.log(response.data.response);
+
+      dataArray = response.data;
+       $scope.totalData = response.data.response;
+        // this callback will be called asynchronously
+        // when the response is available
+      }, function errorCallback(response) {
+
+        console.log('error: ' + response)
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+      });
+
+    }
+
+    $scope.search = function(){
     }
 
 });
